def621e99a5dccb864470d16f7c307b36862a652
Ankit Prakash <ankit.prakash687@gmail.com>
2020-02-07 14:32:11 -0500

    Create MyApp container component

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container'
-import MetricFilter from './components/MetricFilter'
-
-const useStyles = makeStyles({
-    card: {
-      margin: '5% 25%',
-    },
-  });
+import MetricFilter from './Features/MetricFilter/MetricFilter'
+import MetricReading from './Features/MetricReading/MetricReading'
+import MetricGraph from './Features/MetricGraph/MetricGraph'
+import Box from '@material-ui/core/Box'
 
 export default () => {
-    const classes = useStyles();
     return(
         <Container>
+          <div style={{width: '100%'}}>
+            <Box display="flex" >
             <MetricFilter/>
+            </Box>
+            <MetricGraph/>
+            </div>
         </Container>
     )
 }
