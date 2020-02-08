39112f691cbf84a5640cb98558d6353584808eb9
Ankit Prakash <ankit.prakash687@gmail.com>
2020-02-07 19:43:34 -0500

    Update to code clean up

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Container from '@material-ui/core/Container'
 import MetricFilter from './Features/MetricFilter/MetricFilter'
-import MetricReading from './Features/MetricReading/MetricReading'
 import MetricGraph from './Features/MetricGraph/MetricGraph'
 import Box from '@material-ui/core/Box'
+import grey from '@material-ui/core/colors/grey';
 
 export default () => {
     return(
-        <Container>
+        <Container style={{background: grey[100], minHeight: window.innerHeight}}>
           <div style={{width: '100%'}}>
-            <Box display="flex" >
+            <Box display="flex" m={1} p={1} justifyContent="flex-end">
             <MetricFilter/>
             </Box>
             <MetricGraph/>
